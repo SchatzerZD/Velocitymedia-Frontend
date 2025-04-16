edb420bb03606f74e136d3556eeb388ae71c4417
daniel <daniel.walaker@gmail.com>
2025-04-16 13:29:54 +0200

    Created a value for admin and headers for endpoints

--- src/stores/tokenStore.js
+++ src/stores/tokenStore.js
@@ -6,7 +6,13 @@ import { defineStore } from 'pinia'
 export const tokenStore = defineStore("user", () => {
     const user = ref({
         username: "",
-        jwt: ""
+        jwt: "",
+        admin: false
+    })
+
+    const headers = ref({
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer '
     })
 
 
@@ -14,26 +20,47 @@ export const tokenStore = defineStore("user", () => {
         user.value = JSON.parse(localStorage.getItem("user"))
     }
 
+    if (localStorage.getItem("headers")) {
+        headers.value = JSON.parse(localStorage.getItem("headers"))
+    }
+
 
     watch(
         user,
         (userVal) => {
             localStorage.setItem("user", JSON.stringify(userVal));
+
+        },
+        {deep: true}
+    )
+
+    watch(
+        headers,
+        (headerVal) => {
+            localStorage.setItem("headers", JSON.stringify(headerVal));
+
         },
         {deep: true}
     )
 
 
     const changeJWT = (jwt) => {
-        user.value.jwt = jwt;
+        user.value.jwt = jwt
+        headers.value.Authorization = 'Bearer ' + jwt
     }
 
     const changeUsername = (username) => {
         user.value.username = username
+        
+        if(username === 'admin'){
+            user.value.admin = true
+        }else{
+            user.value.admin = false
+        }
     }
 
 
-    return { user, changeJWT, changeUsername }
+    return { user, headers, changeJWT, changeUsername }
 })
 
 
