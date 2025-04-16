400236f7eb8425e477b4ff515e2aada07976225f
daniel <daniel.walaker@gmail.com>
2025-04-16 12:21:34 +0200

    Created components box and row for easier moduling

---
+++ src/components/Box.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <div class="box">
+        <slot></slot>
+    </div>
+
+
+    
+</template>
+
+
+<script>
+
+export default {
+
+}
+
+</script>
+
+
+<style scoped>
+
+.box{
+    background-color: lightgray;
+    width: 17rem;
+    height: 12rem;
+    color: black;
+    border-radius: 1rem;
+    box-shadow: 10px 5px 5px black;
+}
+
+.box:hover{
+    cursor: pointer;
+    scale: 1.1;
+    transition-duration: 0.3s;
+}
+
+</style>
---
+++ src/components/Row.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="row">
+        <slot></slot>
+    </div>
+
+
+</template>
+
+
+<style scoped>
+
+.row{
+    display:flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    flex: 0 0 33.3333%;
+    margin: 0 auto;
+    width: 66%;
+    margin-bottom: 15%;
+    gap: 5rem;
+    justify-content: center;
+}
+
+
+</style>
