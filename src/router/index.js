26580d69487621d08530c3cc36292c36cefd2cfd
daniel <daniel.walaker@gmail.com>
2025-04-16 13:30:03 +0200

    Additional routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import NotFound from '@/views/NotFound.vue'
+import Draft from '@/views/tools/Draft.vue'
+import Log from '@/views/tools/Log.vue'
+import BTS from '@/views/tools/BTS.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +13,23 @@ const router = createRouter({
       name: 'Home',
       component: Home,
     },
+
+    {
+      path: '/bts',
+      name: 'BTS',
+      component: BTS
+    },
+    {
+      path: '/Log',
+      name: 'Log',
+      component: Log
+    },
+    {
+      path: '/draft',
+      name: 'Draft',
+      component: Draft
+    },
+
     {
       path: '/:catchAll(.*)',
       name: 'NotFound',
