e41080ad0ee75264be408c5fd2a590f0593aa849
daniel <daniel.walaker@gmail.com>
2025-04-16 06:26:40 +0200

    Initilized vue for a home page

---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+
+
+<template>
+
+<h1>HOME PAGE</h1>
+
+
+</template>
+
+<script>
+import { tokenStore } from '@/stores/tokenStore';
+
+
+export default {
+
+    data() {
+        return {
+            
+        }
+    },
+    mounted() {
+        console.log(tokenStore().user.jwt)
+        console.log(tokenStore().user.username)
+    }
+
+}
+
+</script>
